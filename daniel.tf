6082ec5462a666380b311afbb4a8907e899a6acf
Daniel Ciuraru <105148851+danielciuraru86@users.noreply.github.com>
2026-06-11 10:13:28 +0300

    Create daniel.tf

---
+++ daniel.tf
@@ -0,0 +1,11 @@
+ingress {
+  from_port  = 0
+  to_port   = 0
+  protocol  = "-1"
+  cidr_blocks = ["1.1.1.1/32"]
+ }
+ egress {
+  from_port  = 0
+  to_port   = 0
+  protocol  = "-1"
+  cidr_blocks = ["1.1.1.1/32"]
